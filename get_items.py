c5e08611e3f515a01905216b6daff6a9d502c40d
Julian Gautier <juliangautier@g.harvard.edu>
2019-10-27 03:55:39 -0400

    Added feedback in terminal
    Added line that prints status, so we know how long the script's going to take.

--- get_items.py
+++ get_items.py
@@ -2,17 +2,18 @@
 
 import urllib2
 import json
-server = '' # Base URL of Dataverse installation, e.g. https://demo.dataverse.org
+server = 'https://dataverse.harvard.edu' # Base URL of Dataverse installation, e.g. https://demo.dataverse.org
 rows = 10
 start = 0
 page = 1
 condition = True
 text_file = open("items.txt", "w") # File with list of items is saved as items.txt
-query = '' # query string for search API, e.g. '*&type=dataverse'
+query = '*&type=dataverse' # query string for search API, e.g. '*&type=dataverse'
 while (condition):
     url = server + '/api/search?q=' + query + "&start=" + str(start)
     data = json.load(urllib2.urlopen(url))
     total = data['data']['total_count']
+    print "start:", start, " total:", total
     for i in data['data']['items']: # path to object in the Search APIs json
         text_file.write(i['identifier'] + "\n") # name of object to collect
     start = start + rows
