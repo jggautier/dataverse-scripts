8ac8a3dcda96578173f45e9d14fadffe3bcd3746
Julian Gautier <juliangautier@g.harvard.edu>
2019-12-13 22:05:56 -0500

    Improved comments and formatting

--- datasetmetadata/get_dataset_pids_and_json_metadata.py
+++ datasetmetadata/get_dataset_pids_and_json_metadata.py
@@ -10,7 +10,7 @@ import csv
 import pandas as pd
 
 # Ask user for URL of dataverse, e.g. https://demo.dataverse.org/dataverse/demo
-dataverse_url = input('\n' + 'Enter full URL of dataverse to add links to:')
+dataverse_url = input('\n' + 'Enter full URL of dataverse:')
 
 # Parse URL to get server name and alias
 parsed = urlparse(dataverse_url)
@@ -21,9 +21,9 @@ alias = parsed.path.split('/')[2]
 
 # create parent directory
 current_directory = os.path.dirname(os.path.realpath(__file__)) # save directory of this python file
-current_time = time.strftime('%Y.%m.%d_%H.%M.%S') # save current time to append it to parent folder name later
-parent_directory_path = os.path.join(current_directory, '%s_dataset_metadata_%s' %(alias, current_time))
-os.mkdir(parent_directory_path)
+current_time = time.strftime('%Y.%m.%d_%H.%M.%S') # save current time to append it to parent folder name
+parent_directory_path = os.path.join(current_directory, '%s_dataset_metadata_%s' %(alias, current_time)) # save parent directory with dataverse alias and current time
+os.mkdir(parent_directory_path) # create current directory
 
 # create directory within parent directory for the JSON metadata files
 metadata_directory_path = os.path.join(parent_directory_path, 'dataset_metadata')
@@ -31,20 +31,22 @@ os.mkdir(metadata_directory_path)
 json_directory_path = os.path.join(parent_directory_path, 'dataset_metadata/json')
 os.mkdir(json_directory_path)
 
-# Use Search API to get PIDs of datasets in the given dataverse
+# Use Search API to get PIDs, persistentUrls and dataverse names of datasets in the given dataverse
 
-# Initative counts of terminal progress indicator
+# Initative counts for pagination and terminal progress indicator
 rows = 10
 start = 0
 condition = True
 
+# Save filename to filename variable
 filename = os.path.join(parent_directory_path,'dataset_pids.csv')
 
 print('Copy dataset PIDs to dataset_pids.csv...')
 
+# Create csv file and create header row
 with open(filename, mode = 'w') as metadatafile:
 	metadatafile = csv.writer(metadatafile, delimiter = ',', quotechar = '"', quoting=csv.QUOTE_MINIMAL)
-	metadatafile.writerow(['global_id', 'persistentUrl', 'dataverse']) # Create header row
+	metadatafile.writerow(['global_id', 'persistentUrl', 'dataverse']) 
 
 rows = 10
 start = 0
@@ -54,25 +56,42 @@ query = '*&type=dataset' # query string for search API, e.g. '*&type=dataverse'
 while (condition):
 	url = server + '/api/search?q=' + query + '&subtree=' + alias + '&start=' + str(start)
 	data = json.load(urlopen(url))
-	total = data['data']['total_count']
+	total = data['data']['total_count'] # Save total count of datasets
+	
 	print('Copied PIDs:', start, 'of total:', total)
 
 	for i in data['data']['items']: # path to object in the Search APIs json
+		
+		# Get global_id
 		global_id = i['global_id']
+		
+		# Get dataset's dataverse name
 		try:
 			dataverse = i['name_of_dataverse']
+			
+		# There's a bug where the Search API output doesn't include the name of a few datasets' dataverses
+		# If there is no dataverse field, save empty value in dataverse variable
 		except KeyError:
 			dataverse = ''
-
+		
+		# Get persistentUrl
 		persistentUrl = i['url']
+		
+		# Write values of the three variables to a new row in the csv
 		with open(filename, mode = 'a') as datasets:
 			def to_utf8(lst):
 				return [unicode(elem).encode('utf-8') for elem in lst] # Convert all characters to utf-8
 			datasets = csv.writer(datasets, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 			datasets.writerow([global_id, persistentUrl, dataverse])
+			
+	# Increase value of start
 	start = start + rows
+	
+	# Set condition to true if start is less then total, so script gets the next page,
+	# or false if start is more than total, which ends the while loop
 	condition = start < total
-print('Copied PIDs:', start, 'of total:', total)
+	
+print('Finished copying PIDs:', start, 'of total:', total)
 
 
 ## ------------------------------------------------------------
@@ -92,13 +111,13 @@ start = 0
 # Save number of items in the dataset_pids list in total variable
 total = len(dataset_pids)
 
-# For each dataset id in the list, download the dataset's Dataverse JSON file into the metadata folder
+# For each dataset pid in the list, download the dataset's Dataverse JSON file into the metadata folder
 for pid in dataset_pids:
-	pid = pid.strip()
-	url = '%s/api/datasets/export?exporter=dataverse_json&persistentId=%s'%(server, pid)
+	pid = pid.strip() # makes sure all white lines are stripped from the pid, otherwise url is invalid
+	url = '%s/api/datasets/export?exporter=dataverse_json&persistentId=%s' %(server, pid)
 	r = requests.get(url)
 	with open(os.path.join(json_directory_path, 'dataset_%s.json' %(pid.split('/')[-1])), mode = 'w') as f:
-		f.write(json.dumps(json.loads(r.content), indent=4, sort_keys=True))
+		f.write(json.dumps(json.loads(r.content), indent = 4, sort_keys = True))
 	print('Downloaded %s of %s JSON files' %(start, total))
 	start += 1
 print('Downloaded %s of %s JSON files' %(total, total))
