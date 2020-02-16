b4d684f55545547db42f7fb83e3ccae630925990
Julian Gautier <juliangautier@g.harvard.edu>
2020-02-15 21:30:26 -0500

    Improved error handling

--- get_dataset_and_file_info.py
+++ get_dataset_and_file_info.py
@@ -1,5 +1,5 @@
 # For non-harvested datasets created within a range of time, get dataset and file info. Useful for spotting problem datasets (e.g. dataset with no data)
-# This script first uses the Search API to find PIDs of datasets.
+# This script first uses the Search API for find PIDs of datasets
 # For each dataset found, the script uses the "get versions" API endpoint to get dataset and file metadata.
 # The script formats and writes that metadata to a CSV file on the users computer
 
@@ -11,26 +11,25 @@ import urllib.request
 from urllib.request import urlopen
 
 # Get required info from user
-server='https://dataverse.harvard.edu' # Dataverse repository must have Search API and "get version" endpoints unblocked
-startdate='' # yyyy-mm-dd
-enddate='' # yyyy-mm-dd
-apikey='' # for getting unpublished datasets accessible to Dataverse account
+server='https://dataverse.harvard.edu'
+startdate='2020-02-13' # yyyy-mm-dd
+enddate='2020-02-14' # yyyy-mm-dd
+apikey='f6fb376a-6244-4b0d-b271-6963c563d02c' # for getting unpublished datasets accessible to Dataverse account
 directory='' # directory for the CSV file containing the dataset and file info, e.g. '/Users/username/Desktop/'
 
 # Initialization for paginating through Search API results
-rows=10
 start=0
-page = 1
+count=0
 condition=True
 
-# List for storing dataset PIDs
+# List for storing indexed dataset PIDs and counting misindexed datasets
 dataset_pids=[]
 
-print('Getting dataset PIDs:')
-
+print('Searching for dataset PIDs:')
 while (condition):
 	try:
-		url='%s/api/search?q=*&fq=metadataSource:"Harvard+Dataverse"&type=dataset&per_page=10&start=%s&sort=date&order=desc&fq=dateSort:[%sT00:00:00Z+TO+%sT23:59:59Z]&key=%s' %(server, start, startdate, enddate, apikey)
+		per_page=10
+		url='%s/api/search?q=*&fq=metadataSource:"Harvard+Dataverse"&type=dataset&per_page=%s&start=%s&sort=date&order=desc&fq=dateSort:[%sT00:00:00Z+TO+%sT23:59:59Z]&key=%s' %(server, per_page, start, startdate, enddate, apikey)
 		data=json.load(urlopen(url))
 
 		# Get total number of results
@@ -43,24 +42,42 @@ while (condition):
 			global_id=i['global_id']
 			dataset_pids.append(global_id)
 
+		print('Datasets found: %s of %s (10 at a time)' %(len(dataset_pids), total))
+
 		# Update variables to paginate through the search results
-		start=start + rows
-		page+=1
+		start=start+per_page
 
 		# Stop paginating when there are no more results
-		condition=start < total
-
-		# As a progress indicator, print a dot for each page
-		sys.stdout.write('.')
-		sys.stdout.flush()
+		condition=start<total
 
 	# Print error message if misindexed datasets break the Search API call, and try the next page. (See https://github.com/IQSS/dataverse/issues/4225)
 	except urllib.error.URLError:
-		print('\nCould not get %s datasets. A dataset indexing error is preventing the following URL from loading:\n%s\n' %(rows, url))
-		start=start + rows
-		page+=1
+		misindexed_datasets_count=0
+		try:
+			per_page=1
+			url='%s/api/search?q=*&fq=metadataSource:"Harvard+Dataverse"&type=dataset&per_page=%s&start=%s&sort=date&order=desc&fq=dateSort:[%sT00:00:00Z+TO+%sT23:59:59Z]&key=%s' %(server, per_page, start, startdate, enddate, apikey)
+			data=json.load(urlopen(url))
 
-print('\nFound %s of %s datasets' %(len(dataset_pids), total))
+			# Get total number of results
+			total=data['data']['total_count']
+			
+			# Get dataset PID and save to dataset_pids list
+			global_id=data['data']['items'][0]['global_id']
+			dataset_pids.append(global_id)
+
+			print('Datasets found: %s of %s (1 at a time)' %(len(dataset_pids), total))
+
+			# Update variables to paginate through the search results
+			start=start+per_page
+
+			# Stop paginating when there are no more results
+			condition=start<total
+
+		except urllib.error.URLError:
+			misindexed_datasets_count+=1			
+			start=start+per_page
+
+print('Datasets misindexed: %s' %(misindexed_datasets_count))
 
 # Store name of csv file, which includes the dataset start and end date range, to the 'filename' variable
 filename='datasetinfo_%s-%s.csv' %(startdate.replace('-', '.'), enddate.replace('-', '.'))
@@ -69,11 +86,11 @@ filename='datasetinfo_%s-%s.csv' %(startdate.replace('-', '.'), enddate.replace(
 csvfile=os.path.join(directory, filename)
 
 # Create CSV file
-with open(csvfile, mode='w') as writecsvfile:
-	writecsvfile=csv.writer(writecsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+with open(csvfile, mode='w') as opencsvfile:
+	opencsvfile=csv.writer(opencsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	
 	# Create header row
-	writecsvfile.writerow(['datasetURL (publication state)', 'ds_title', 'fileinfo'])
+	opencsvfile.writerow(['datasetURL (publication state)', 'ds_title', 'fileinfo'])
 
 # For each data file in each dataset, add to the CSV file the dataset's URL and publication state, dataset title, data file name and data file contentType
 
@@ -102,16 +119,16 @@ for pid in dataset_pids:
 			fileinfo='%s (%s bytes; %s)' %(filename, filesize, contentType)
 
 			# Append fields to the csv file
-			with open(csvfile, mode='a') as writecsvfile:
+			with open(csvfile, mode='a') as opencsvfile:
 		
 				# Convert all characters to utf-8
 				def to_utf8(lst):
 					return [unicode(elem).encode('utf-8') for elem in lst]
 
-				writecsvfile=csv.writer(writecsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+				opencsvfile=csv.writer(opencsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 				
 				# Create new row with dataset and file info
-				writecsvfile.writerow([datasetURL_pubstate, ds_title, fileinfo])
+				opencsvfile.writerow([datasetURL_pubstate, ds_title, fileinfo])
 
 				# As a progress indicator, print a dot each time a row is written
 				sys.stdout.write('.')
@@ -119,16 +136,16 @@ for pid in dataset_pids:
 
 	# Otherwise print that the dataset has no files
 	else:
-		with open(csvfile, mode='a') as writecsvfile:
+		with open(csvfile, mode='a') as opencsvfile:
 		
 			# Convert all characters to utf-8
 			def to_utf8(lst):
 				return [unicode(elem).encode('utf-8') for elem in lst]
 
-			writecsvfile=csv.writer(writecsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			opencsvfile=csv.writer(opencsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 			
 			# Create new row with dataset and file info
-			writecsvfile.writerow([datasetURL_pubstate, ds_title, '(no files found)'])
+			opencsvfile.writerow([datasetURL_pubstate, ds_title, '(no files found)'])
 
 			# As a progress indicator, print a dot each time a row is written
 			sys.stdout.write('.')
