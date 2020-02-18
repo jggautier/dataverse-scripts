341684d2560116fea0bd3629d4c01291ea5fe701
jggautier <jggautier@ucla.edu>
2020-02-18 01:16:48 -0500

    Added checkbox to let user include subdataverses

--- get_dataset_PIDs.py
+++ get_dataset_PIDs.py
@@ -17,7 +17,7 @@ from urllib.parse import urlparse
 # Create GUI for getting user input
 window=Tk()
 window.title('Get dataset PIDs')
-window.geometry('475x275') # width x height
+window.geometry('475x450') # width x height
 
 # Function called when Browse button is pressed
 def retrieve_directory():
@@ -28,11 +28,19 @@ def retrieve_directory():
 
 	# Show user which directory she chose
 	label_showChosenDirectory=Label(window, text='You chose: ' + directory, anchor='w', foreground='green')
-	label_showChosenDirectory.grid(sticky='w', column=0, row=7)
+	label_showChosenDirectory.grid(sticky='w', column=0, row=13)
 
 # Function called when Start button is pressed
 def retrieve_input():
 	global dataverseUrl
+	global apikey
+	global get_subdataverses
+
+	# Record if user wants to search in subdataverses
+	get_subdataverses=get_subdataverses.get()
+
+	# Store what entered in the api key text box as a global variable
+	apikey=entry_apikey.get()
 
 	# Store what's entered in dataverseUrl text box as a global variable
 	dataverseUrl=entry_dataverseUrl.get()
@@ -61,24 +69,46 @@ dataverseUrl=str()
 entry_dataverseUrl=Entry(window, width=50, textvariable=dataverseUrl)
 entry_dataverseUrl.grid(sticky='w', column=0, row=1, pady=2)
 
-# Create help text before URL field
+# Create help text for Dataverse URL field
 label_dataverseUrlHelpText=Label(window, text='Example: https://demo.dataverse.org/dataverse/dataversealias', foreground='grey', anchor='w')
 label_dataverseUrlHelpText.grid(sticky='w', column=0, row=2)
 
 # Create empty row in grid to improve spacing between the two fields
 window.grid_rowconfigure(4, minsize=25)
 
+get_subdataverses=IntVar()
+c=Checkbutton(window, text="Include subdataverses", variable=get_subdataverses).grid(sticky='w', column=0, row=5)
+
+# Create empty row in grid to improve spacing between the two fields
+window.grid_rowconfigure(6, minsize=25)
+
+# Create label for API key field
+label_apikey=Label(window, text='API key:', anchor='w')
+label_apikey.grid(sticky='w', column=0, row=7)
+
+# Create API key field
+apikey=str()
+entry_apikey=Entry(window, width=50, textvariable=apikey)
+entry_apikey.grid(sticky='w', column=0, row=8, pady=2)
+
+# Create help text for API key field
+label_apikeyHelpText=Label(window, text='If no API is entered, only published datasets will be found', foreground='grey', anchor='w')
+label_apikeyHelpText.grid(sticky='w', column=0, row=9)
+
+# Create empty row in grid to improve spacing between the two fields
+window.grid_rowconfigure(10, minsize=25)
+
 # Create label for Browse directory button
 label_browseDirectory=Label(window, text='Choose folder to store list of dataset PIDs:', anchor='w')
-label_browseDirectory.grid(sticky='w', column=0, row=5, pady=2)
+label_browseDirectory.grid(sticky='w', column=0, row=11, pady=2)
 
 # Create Browse directory button
 button_browseDirectory=ttk.Button(window, text='Browse', command=lambda: retrieve_directory())
-button_browseDirectory.grid(sticky='w', column=0, row=6)
+button_browseDirectory.grid(sticky='w', column=0, row=12)
 
 # Create start button
 button_Submit=ttk.Button(window, text='Start', command=lambda: retrieve_input())
-button_Submit.grid(sticky='w', column=0, row=8, pady=40)
+button_Submit.grid(sticky='w', column=0, row=14, pady=40)
 
 # Keep window open until it's closed
 mainloop()
@@ -94,14 +124,10 @@ current_time=time.strftime('%Y.%m.%d_%H.%M.%S')
 ####################################################################################
 # Get IDs of dataverses within the given dataverse and save as a list with parent dataverse - excludes linked dataverses
 
-server=''
-alias=''
-
 # To get unpublished dataverses and datasets, user needs to provide api key of an authoritized Dataverse account
-apikey=''
 # apikey=''
 
-get_subdataverses='' # Enter 'yes' or 'no'
+# get_subdataverses=1
 
 # Get ID of given dataverse alias
 if apikey:
@@ -110,18 +136,17 @@ else:
 	url='%s/api/dataverses/%s' %(server, alias)
 
 data=json.load(urlopen(url))
-dataverse_id=data['data']['id']
+parent_dataverse_id=data['data']['id']
 
 # Create list and add ID of given dataverse
-dataverse_ids=[dataverse_id]
+dataverse_ids=[parent_dataverse_id]
 
 
 # If user wants datasets in subdataverses, search for and include IDs of subdataverses
 
 # Get each sub-dataverse in the given dataverse
-if get_subdataverses=='yes': # If user indicates that she wants subdataverses...
+if get_subdataverses==1: # If user indicates that she wants subdataverses...
 	print('Getting dataverse IDs in %s:' %(alias))
-	count=1
 
 	for dataverse_id in dataverse_ids:
 		# As a progress indicator, print a dot each time a row is written
@@ -136,33 +161,42 @@ if get_subdataverses=='yes': # If user indicates that she wants subdataverses...
 			if i['type']=='dataverse':
 				dataverse_id=i['id']
 				dataverse_ids.extend([dataverse_id])
-				count+=1
-	print('\n%s dataverse IDs saved' %(len(dataverse_ids)))
+
+	print('\nFound 1 dataverse and %s subdataverses' %(len(dataverse_ids)-1))
 
 ####################################################################################
-# For each dataverse in the list, get the PIDs of all datasets - excludes linked and harvested datasets
+# For each dataverse in the list, add the PIDs of all datasets to a text file - excludes linked and harvested datasets
 
-print('Getting dataset IDs:')
+txtfile='dataset_pids-%s(%s).txt' %(alias, current_time)
+txtfilepath=os.path.join(directory, txtfile)
+
+print('Writing dataset IDs to %s:' %(txtfilepath))
 
 count=0
-for id in dataverse_ids:
-	if apikey:
-		url='%s/api/dataverses/%s/contents?key=%s' %(server, id, apikey)
-	else:
-		url='%s/api/dataverses/%s/contents' %(server, id)
-	data=json.load(urlopen(url))
 
-	for i in data['data']:
-		if i['type']=='dataset':
-			protocol=i['protocol']
-			authority=i['authority']
-			identifier=i['identifier']
-			print('%s:%s/%s' %(protocol, authority, identifier))
+with open(txtfilepath, mode='w') as opentxtfile:
+	for id in dataverse_ids:
+		if apikey:
+			url='%s/api/dataverses/%s/contents?key=%s' %(server, id, apikey)
+		else:
+			url='%s/api/dataverses/%s/contents' %(server, id)
+		data=json.load(urlopen(url))
 
-			# As a progress indicator, print a dot each time a row is written
-			# sys.stdout.write('.')
-			# sys.stdout.flush()
+		for i in data['data']:
+			if i['type']=='dataset':
+				protocol=i['protocol']
+				authority=i['authority']
+				identifier=i['identifier']
+				dataset_pid='%s:%s/%s' %(protocol, authority, identifier)
+				
+				count+=1
 
-			count+=1
+				# Create new row with dataset and file info
+				opentxtfile.write('%s\n' %(dataset_pid))
 
-print('Datasets saved: %s' %(count))
+				# As a progress indicator, print a dot each time a row is written
+				sys.stdout.write('.')
+				sys.stdout.flush()
+
+
+print('\nDatasets saved: %s' %(count))
