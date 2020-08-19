5ec56b74dc45cd553b4ab6b1626b080b7748e73f
jggautier <jggautier@ucla.edu>
2020-08-19 11:57:02 -0400

    Update parse_metadatablock_json_files.py
    Adjusted script so that it expects a directory of the folders and files created by the get_dataset_metadata_of_all_installations.py script. Improved some commenting

--- parse_metadatablock_json_files.py
+++ parse_metadatablock_json_files.py
@@ -1,4 +1,7 @@
-# For the metadatablocks in a given directory, parse the metadata fields into a CSV file for further analysis.
+'''
+This script takes the metadatablock files retrieved by the get_dataset_metadata_of_all_installations.py script
+and adds the metadatablock names and fields of each installation into a CSV file for further analysis.
+'''
 
 import csv
 import json
@@ -6,38 +9,44 @@ import os
 from pathlib import Path
 import re
 
-# Enter path to directory containing metadatablock JSON files
-metadatablocks_folder = ''
+# Enter path to directory that contains the folders and files created by the get_dataset_metadata_of_all_installations.py script
+main_directory = ''
 
-# Enter path to directory to store CSV file
+# Enter path to directory to store CSV file that this script will create
 csvfile_folder = ''
 
 csvfile = str(Path(csvfile_folder + '/' + 'metadatablocks.csv'))
+
 with open(csvfile, mode='w') as data:
     data = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
     # Create header row
     data.writerow(['installation_name_(Dataverse_version)', 'metadatablock_name', 'parentfield_name', 'subfield_name'])
 
-for repository_name in os.listdir(metadatablocks_folder):
+for repository_name in os.listdir(main_directory):
 
     # Get the repository name
     repository_name = repository_name
 
-    # Open each folder
-    repository_folder_path = str(Path(metadatablocks_folder + '/' + repository_name))
+    # Open each installation folder
+    repository_folder_path = str(Path(main_directory + '/' + repository_name))
     if os.path.isdir(repository_folder_path):
-        real_repository_folder_path = repository_folder_path
-        print(repository_name)
+        # real_repository_folder_path = repository_folder_path
+
+        for sub_folder in os.listdir(repository_folder_path):
+            if 'metadatablocks' in sub_folder:
+                metadatablock_folder_path = str(Path(repository_folder_path + '/' + sub_folder))
 
         # Open each .json file
-        for metadatablock_file in os.listdir(real_repository_folder_path):
+        for metadatablock_file in os.listdir(metadatablock_folder_path):
             metadatablock_name_version = metadatablock_file
 
             # Get only the metadatablock name from the name of each metadatablock JSON file
             metadatablock_name = re.search(r'^([^()])+', metadatablock_name_version).group()
             metadatablock_name = metadatablock_name.rstrip('_')
 
-            metadatablock_file_path = str(Path(repository_folder_path + '/' + metadatablock_file))
+            metadatablock_file_path = str(Path(metadatablock_folder_path + '/' + metadatablock_file))
+
             with open(metadatablock_file_path, 'r') as f:  # Open file in read mode
                 metadatablock_data = f.read()  # Copy content to dataset_metadata variable
                 metadatablock_data = json.loads(metadatablock_data)  # Load content as a python dict
@@ -83,7 +92,7 @@ for repository_name in os.listdir(metadatablocks_folder):
                 for primitive_field in primitive_fields:
 
                     # Set subfield to an empty string so that Dataverse ingests the CSV file.
-                    # (Dataverse doesn't seem it when there is nothing entered in the fourth column)
+                    # (Dataverse's ingest process doesn't seem to like it when there is nothing entered in the fourth column)
                     subfield = ''
                     with open(csvfile, mode='a') as data:
                         data = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
