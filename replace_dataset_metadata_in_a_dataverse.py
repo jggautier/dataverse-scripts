5f372e32e824cbe14b906f3b43c950970306f062
jggautier <jggautier@ucla.edu>
2020-11-16 16:30:46 -0500

    Create replace_dataset_metadata_in_a_dataverse.py

---
+++ replace_dataset_metadata_in_a_dataverse.py
@@ -0,0 +1,36 @@
+# Replace dataset metadata in given datasets
+
+from csv import DictReader
+import requests
+
+token = ''  # Enter API token of Dataverse account that has edit and publish privileges on the datasets.
+server = ''  # Enter name of server url, which is home page URL of the Dataverse installation, e.g. https://demo.dataverse.org
+
+metadatafile = ''  # Path to json file that contains the replacement metadata
+datasetPIDs = ''  # File with list of dataset PIDs
+
+count = 0
+with open(datasetPIDs, mode='r', encoding='utf-8') as f:
+    total = len(f.readlines()) - 1
+
+with open(datasetPIDs, mode='r', encoding='utf-8') as f:
+    csv_dict_reader = DictReader(f, delimiter=',')
+    for row in csv_dict_reader:
+        datasetPID = row['persistent_id'].rstrip()
+        url = '%s/api/datasets/:persistentId/editMetadata' % (server)
+        r = requests.put(
+            url,
+            data=open(metadatafile, 'rb'),
+            params={
+                'persistentId': datasetPID,
+                'replace': 'true'
+            },
+            headers={
+                'X-Dataverse-key': token
+            })
+        count += 1
+
+        if r.status_code == 200:
+            print('Success: %s! %s of %s' % (datasetPID, count, total))
+        else:
+            print('Failed: %s! %s of %s' % (datasetPID, count, total))
