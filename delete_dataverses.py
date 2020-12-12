20e6af224ee294a840cf8921410239fe9f45ad05
jggautier <jggautier@ucla.edu>
2020-12-11 23:11:44 -0500

    Update delete_dataverses.py
    Adding more comments

--- delete_dataverses.py
+++ delete_dataverses.py
@@ -1,4 +1,7 @@
-# Delete dataverses
+'''
+Delete the dataverses in a given .txt file or CSV file that includes
+a list or column of dataverse aliases
+'''
 
 from csv import DictReader
 import urllib.request
@@ -7,6 +10,7 @@ file = ''  # Path to file with aliases of dataverses to be deleted
 server = ''  # Base URL of repository hosting the dataverses to be deleted
 apikey = ''  # Superuser API key
 
+# Read lines in .txt file or column named 'dataverse_alias' in .csv file
 dataverseAliases = []
 
 if '.txt' in file:
@@ -26,11 +30,12 @@ if '.csv' in file:
             dataverseAlias = dataverse['dataverse_alias']
             dataverseAliases.append(dataverseAlias)
 
+# Create variables for the script's progress
 deletedDataverses = []
 notDeletedDataverses = []
-
 count = 0
 
+# For each dataverseAlias in dataverseAliases list...
 for dataverseAlias in dataverseAliases:
     count += 1
 
@@ -43,14 +48,18 @@ for dataverseAlias in dataverseAliases:
         headers=headers,
         method='DELETE')
 
+    # Try to delete the dataverse
     try:
         response = urllib.request.urlopen(req)
         print('%s of %s: Deleted - %s' % (count, total, dataverseAlias))
         deletedDataverses.append(dataverseAlias)
+
+    # Or save alias of dataverse that failes to delete
     except Exception:
         print('%s of %s: Could not be deleted - %s' % (count, total, dataverseAlias))
         notDeletedDataverses.append(dataverseAlias)
 
+# Print results
 print('Dataverses deleted: %s' % (len(deletedDataverses)))
 
 if notDeletedDataverses:
