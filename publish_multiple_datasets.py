7c553288ad9f7a86d789eb00222c7b5f8c7455b6
jggautier <jggautier@ucla.edu>
2020-12-17 11:36:38 -0500

    Update publish_multiple_datasets.py

--- publish_multiple_datasets.py
+++ publish_multiple_datasets.py
@@ -1,13 +1,15 @@
-# Publish a given list of datasets
+# Publish a given list of draft datasets
 
 import csv
 from csv import DictReader
 import requests
 
-repositoryURL = 'https://demo.dataverse.org'
+repositoryURL = ''  # Base URL of the Dataverse repository, e.g. https://demo.dataverse.org
 apikey = ''
-datasetPIDFile = ''
-type = ''
+datasetPIDFile = ''  # Path to .csv or .txt file containing dataset PIDs
+versionType = ''  # Enter "major" or "minor".
+# To learn about major and minor versions,
+# see https://guides.dataverse.org/en/latest/user/dataset-management.html#dataset-versions
 
 datasetPIDs = []
 if '.csv' in datasetPIDFile:
@@ -32,7 +34,7 @@ elif '.txt' in datasetPIDFile:
 count = 0
 for datasetPID in datasetPIDs:
     url = '%s/api/datasets/:persistentId/actions/:publish' % (repositoryURL)
-    params = {'persistentId': datasetPID, 'type': type}
+    params = {'persistentId': datasetPID, 'type': versionType}
     r = requests.post(
         url,
         params=params,
