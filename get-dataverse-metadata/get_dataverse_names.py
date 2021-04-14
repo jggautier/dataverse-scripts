03750ac76ced754a1de674cbcdc16582a5b6e1a7
jggautier <jggautier@ucla.edu>
2021-04-14 09:35:24 -0400

    Update get_dataverse_names.py

--- get-dataverse-metadata/get_dataverse_names.py
+++ get-dataverse-metadata/get_dataverse_names.py
@@ -1,20 +1,17 @@
-# Python script for getting the dataverse names of given dataset PIDs
+# Python script for getting the Dataverse collection names of given dataset PIDs
 
 import csv
-import json
+from csv import DictReader
 import os
-# from pyDataverse.api import Api
-# import time
+import requests
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
-from urllib.request import urlopen
-# from urllib.parse import urlparse
 
 # Create GUI for getting user input
 window = Tk()
-window.title('Get dataverse names')
-window.geometry('650x400')  # width x height
+window.title('Get Dataverse collection names')
+window.geometry('500x600')  # width x height
 
 # Create label for Dataverse repository URL
 label_repositoryURL = Label(window, text='Enter Dataverse repository URL:', anchor='w')
@@ -32,40 +29,56 @@ label_dataverseUrlHelpText.grid(sticky='w', column=0, row=2)
 # Create empty row in grid to improve spacing between the two fields
 window.grid_rowconfigure(3, minsize=25)
 
-# Create label for Browse directory button
-label_browseForFile = Label(window, text='Choose txt file contain list of dataset PIDs:', anchor='w')
-label_browseForFile.grid(sticky='w', column=0, row=4, pady=2)
+# Create label for API key field
+labelApikey = Label(window, text='API token/key:', anchor='w')
+labelApikey.grid(sticky='w', column=0, row=4)
 
-# Create Browse directory button
-button_browseForFile = ttk.Button(window, text='Browse', command=lambda: retrieve_file())
-button_browseForFile.grid(sticky='w', column=0, row=5)
+# Create API key field
+apikey = str()
+entryApikey = Entry(window, width=50, textvariable=apikey)
+entryApikey.grid(sticky='w', column=0, row=5, pady=2)
+
+# Create help text for API key field
+labelApikeyHelpText = Label(window, text='If no API token/key is entered, only published metadata will be downloaded', foreground='grey', anchor='w')
+labelApikeyHelpText.grid(sticky='w', column=0, row=6)
 
 # Create empty row in grid to improve spacing between the two fields
 window.grid_rowconfigure(7, minsize=25)
 
 # Create label for Browse directory button
-label_browseDirectory = Label(window, text='Choose folder to put csv file into:', anchor='w')
-label_browseDirectory.grid(sticky='w', column=0, row=8, pady=2)
+label_browseForFile = Label(window, text='Choose CSV or TXT file containing list of dataset PIDs:', anchor='w')
+label_browseForFile.grid(sticky='w', column=0, row=8, pady=2)
+
+# Create Browse directory button
+button_browseForFile = ttk.Button(window, text='Browse', command=lambda: retrieve_file())
+button_browseForFile.grid(sticky='w', column=0, row=9)
+
+# Create empty row in grid to improve spacing between the two fields
+window.grid_rowconfigure(11, minsize=25)
+
+# Create label for Browse directory button
+label_browseDirectory = Label(window, text='Choose folder to put CSV file into:', anchor='w')
+label_browseDirectory.grid(sticky='w', column=0, row=12, pady=2)
 
 # Create Browse directory button
 button_browseDirectory = ttk.Button(window, text='Browse', command=lambda: retrieve_directory())
-button_browseDirectory.grid(sticky='w', column=0, row=9)
+button_browseDirectory.grid(sticky='w', column=0, row=13)
 
 # Create start button
 button_Submit = ttk.Button(window, text='Start', command=lambda: retrieve_input())
-button_Submit.grid(sticky='w', column=0, row=11, pady=40)
+button_Submit.grid(sticky='w', column=0, row=15, pady=40)
 
 
-# Function called when Browse button is pressed for choosing text file with dataset PIDs
+# Function called when Browse button is pressed for choosing file with dataset PIDs
 def retrieve_file():
-    global dataset_pids
+    global datasetPIDFile
 
     # Call the OS's file directory window and store selected object path as a global variable
-    dataset_pids = filedialog.askopenfilename(filetypes=[('Text files', '*.txt')])
+    datasetPIDFile = filedialog.askopenfilename(filetypes=[('Text files', '*.txt'), ('CSV files', '*.csv')])
 
     # Show user which file she chose
-    label_showChosenFile = Label(window, text='You chose: ' + dataset_pids, anchor='w', foreground='green', wraplength=500, justify='left')
-    label_showChosenFile.grid(sticky='w', column=0, row=6)
+    label_showChosenFile = Label(window, text='You chose: ' + datasetPIDFile, anchor='w', foreground='green', wraplength=500, justify='left')
+    label_showChosenFile.grid(sticky='w', column=0, row=10)
 
 
 # Function called when Browse button is pressed
@@ -77,7 +90,7 @@ def retrieve_directory():
 
     # Show user which directory she chose
     label_showChosenDirectory = Label(window, text='You chose: ' + csvDirectory, anchor='w', foreground='green', wraplength=500, justify='left')
-    label_showChosenDirectory.grid(sticky='w', column=0, row=10)
+    label_showChosenDirectory.grid(sticky='w', column=0, row=14)
 
 
 # Function called when Start button is pressed
@@ -93,46 +106,62 @@ def retrieve_input():
 # Keep window open until it's closed
 mainloop()
 
+datasetPIDs = []
+if '.csv' in datasetPIDFile:
+    with open(datasetPIDFile, mode='r', encoding='utf-8') as f:
+        csvDictReader = DictReader(f, delimiter=',')
+        for row in csvDictReader:
+            datasetPIDs.append(row['persistent_id'].rstrip())
 
-filename = os.path.join(csvDirectory, 'dataversenames.csv')
+elif '.txt' in datasetPIDFile:
+    datasetPIDFile = open(datasetPIDFile)
+    for datasetPID in datasetPIDFile:
+
+        # Remove any trailing spaces from datasetPID
+        datasetPIDs.append(datasetPID.rstrip())
+
+total = len(datasetPIDs)
+
+filename = os.path.join(csvDirectory, 'dataverse_names.csv')
 
 # Create CSV file
 with open(filename, mode='w') as opencsvfile:
     opencsvfile = csv.writer(opencsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     # Create header row
-    opencsvfile.writerow(['dataset_id', 'datasetUrl', 'dataverseAlias', 'dataverseName'])
+    opencsvfile.writerow(['persistentUrl', 'dataverseAlias', 'dataverseName'])
 
-dataset_pids = open(dataset_pids)
 
-piderrors = []
-apikey = ''
-for pid in dataset_pids:
+apikey = 'ba0ff942-6623-4f46-ae4d-42b42998327f'
+
+datasetPIDErrors = []
+count = 0
+for datasetPID in datasetPIDs:
     # Construct "Get Versions" API endpoint url
     try:
         if apikey:
-            url = '%s/api/search?q="%s"&type=dataset&show_entity_ids=true&key=%s' % (repositoryURL, pid, apikey)
+            url = '%s/api/search?q="%s"&type=dataset&show_entity_ids=true&key=%s' % (repositoryURL, datasetPID, apikey)
         else:
-            url = '%s/api/search?q="%s"&type=dataset&show_entity_ids=true' % (repositoryURL, pid)
+            url = '%s/api/search?q="%s"&type=dataset&show_entity_ids=true' % (repositoryURL, datasetPID)
         # Store dataset and file info from API call to "data" variable
-        data = json.load(urlopen(url))
-    except urllib.error.URLError:
-        piderrors.append(pid)
+        response = requests.get(url)
+        data = response.json()
 
-    # Save dataset PID and dataverse name
-    dataset_id = data['data']['items'][0]['entity_id']
-    persistentUrl = data['data']['items'][0]['url']
-    dataverseName = data['data']['items'][0]['name_of_dataverse']
-    dataverseAlias = data['data']['items'][0]['identifier_of_dataverse']
+        # Save dataset PID and dataverse name
+        persistentUrl = data['data']['items'][0]['url']
+        dataverseName = data['data']['items'][0]['name_of_dataverse']
+        dataverseAlias = data['data']['items'][0]['identifier_of_dataverse']
 
-    # Write values of the three variables to a new row in the CSV
-    with open(filename, mode='a') as datasets:
+        # Write values of the three variables to a new row in the CSV
+        with open(filename, mode='a') as datasets:
+            datasets = csv.writer(datasets, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            datasets.writerow([persistentUrl, dataverseAlias, dataverseName])
+        count += 1
 
-        # Convert all characters to utf-8
-        def to_utf8(lst):
-            return [unicode(elem).encode('utf-8') for elem in lst]
+    except Exception:
+        datasetPIDErrors.append(datasetPID)
 
-        datasets = csv.writer(datasets, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        datasets.writerow([dataset_id, persistentUrl, dataverseAlias, dataverseName])
-
-print('Copied PIDs:', total, 'of total:', total)
+    print('Dataverse collection names retrieved: %s of %s (%s)' % (count, total, datasetPID))
+if datasetPIDErrors:
+    print('The Dataverse collection names of these datasets could not be retrieved:\n')
+    print(datasetPIDErrors)
