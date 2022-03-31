37d4f954a8ec4bff05de4a5c6a3b008f53e7e4d6
Julian Gautier <jggautier@ucla.edu>
2022-03-31 10:47:14 -0400

    Update delete_dataset_locks.py

--- delete_dataset_locks.py
+++ delete_dataset_locks.py
@@ -8,6 +8,7 @@ apikey = ''  # API key of superuser account
 file = ''  # Path to .txt or .csv file with database IDs of dataverses to be deleted
 
 datasetPIDs = []
+
 if '.csv' in file:
     with open(file, mode='r', encoding='utf-8') as f:
         csvDictReader = DictReader(f, delimiter=',')
@@ -35,6 +36,6 @@ for datasetPID in datasetPIDs:
     count += 1
 
     if req.status_code == 200:
-        print('Success: %s! %s of %s' % (datasetPID, count, total))
+        print('Success!: %s %s of %s' % (datasetPID, count, total))
     else:
-        print('Failure: %s! %s of %s' % (datasetPID, count, total))
+        print('Failure: %s %s of %s' % (datasetPID, count, total))
