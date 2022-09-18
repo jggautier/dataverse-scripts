4abeaab5f9c05e9adf4d0009f08e0d495ab0bedf
Julian Gautier <jggautier@ucla.edu>
2022-09-17 18:47:41 -0700

    Update dataverse_repository_curation_assistant_functions.py

--- dataverse_repository_curation_assistant/dataverse_repository_curation_assistant_functions.py
+++ dataverse_repository_curation_assistant/dataverse_repository_curation_assistant_functions.py
@@ -1255,3 +1255,274 @@ def delete_published_datasets(
             notDeletedMessage = 'Datasets not deleted: %s' % (len(notDestroyedDatasets))
             notDeletedText.set(notDeletedMessage)
             rootWindow.update_idletasks()
+
+def save_locked_dataset_report(installationUrl, apiKey, directoryPath):
+    # List lock types. See https://guides.dataverse.org/en/5.10/api/native-api.html?highlight=locks#list-locks-across-all-datasets
+    lockTypesList = ['Ingest', 'finalizePublication']
+
+    currentTime = time.strftime('%Y.%m.%d_%H.%M.%S')
+
+    lockedDatasetPids = []
+    lockTypesString = list_to_string(lockTypesList)
+
+    # Get dataset PIDs of datasets that have any of the lock types in lockTypesList
+    # print(f'Getting information about datasets with the lock types: {lockTypesString}')
+    for lockType in lockTypesList:
+
+        datasetLocksApiEndpoint = f'{installationUrl}/api/datasets/locks?type={lockType}'
+        response = requests.get(
+            datasetLocksApiEndpoint,
+            headers={'X-Dataverse-key': apiKey})
+        data = response.json()
+
+        if data['status'] == 'OK':
+            for lock in data['data']:
+                lockedDatasetPid = lock['dataset']
+                lockedDatasetPids.append(lockedDatasetPid)
+
+    # Remove PIDs in ignorePIDs list from lockedDatasetPids list
+    lockedDatasetPids = [lockedDatasetPid for lockedDatasetPid in lockedDatasetPids if lockedDatasetPid not in ignorePIDs]
+
+    # Use set function to deduplicate lockedDatasetPids list and convert set to a list again
+    lockedDatasetPids = list(set(lockedDatasetPids))
+
+    total = len(lockedDatasetPids)
+
+    # Create CSV file and header row
+    csvOutputFile = f'dataset_locked_status_{currentTime}.csv'
+    csvOutputFilePath = os.path.join(directoryPath, csvOutputFile)
+
+    with open(csvOutputFilePath, mode='w', newline='') as f:
+        f = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        f.writerow([
+            'dataset_url', 'dataset_title', 'lock_reason', 'locked_date', 'user_name',
+            'contact_email', 'possible_duplicate_datasets', 'rt_ticket_urls'])
+
+        if total == 0:
+            datasetUrl='No locked datasets found', 
+            lockedDatasetTitle = 'N/A' 
+            reason = 'N/A' 
+            lockedDate = 'N/A' 
+            userName = 'N/A'
+            contactEmailsString = 'N/A' 
+            potentialDuplicateDatasetsString = 'N/A' 
+            # rtTicketUrlsString='N/A'
+
+            f.writerow([
+                datasetUrl, lockedDatasetTitle, reason, lockedDate, userName,
+                contactEmailsString, potentialDuplicateDatasetsString])
+
+            # print(f'No locked datasets found (not including the {len(ignorePIDs)} dataset(s) being ignored).')
+
+        elif total > 0:
+            # print(f'Locked datasets found: {total}\r\r')
+
+            lockedDatasetCount = 0
+
+            # For each dataset, write to the CSV file info about each lock the dataset has
+            for lockedDatasetPid in lockedDatasetPids:
+                lockedDatasetCount += 1
+
+                # print(f'\rGetting information about {lockedDatasetCount} of {total} datasets: {lockedDatasetPid}')
+
+                # print('\tGetting dataset title and contact information')
+                datasetMetadata = get_dataset_metadata_export(
+                    installationUrl=installationUrl, datasetPid=lockedDatasetPid, 
+                    exportFormat='dataverse_json', apiKey=apiKey)
+
+                # Get title of latest version of the dataset
+                for field in datasetMetadata['data']['latestVersion']['metadataBlocks']['citation']['fields']:
+                    if field['typeName'] == 'title':
+                        lockedDatasetTitle = field['value']
+
+                # Get contact email addresses of the dataset
+                contactEmailsList = []
+
+                for field in datasetMetadata['data']['latestVersion']['metadataBlocks']['citation']['fields']:
+                    if field['typeName'] == 'datasetContact':
+                        for contact in field['value']:
+                            contactEmail = contact['datasetContactEmail']['value']
+                            contactEmailsList.append(contactEmail)
+                contactEmailsString = list_to_string(contactEmailsList)
+
+                # # If RT username and password is provided, log into RT and use the contact email addresses to
+                # # search for support emails from the dataset depositor
+                # if rtUserLogin and rtUserPassword != '':
+                #     import pkg_resources
+                #     pkg_resources.require('rt==2.1.1')
+                #     import rt
+                #     # Log in to RT to search for support emails from the dataset depositors
+                #     # print('\tLogging into RT support email system')
+                #     tracker = rt.Rt('https://help.hmdc.harvard.edu/REST/1.0/', rtUserLogin, rtUserPassword)
+                #     tracker.login()
+
+                #     # print('\tSearching for related emails in the RT support email system')
+                #     rtTicketUrlsList = []
+                #     for contactEmail in contactEmailsList:
+
+                #         # Search RT system for emails sent from the contact email address
+                #         searchResults = tracker.search(
+                #             Queue='dataverse_support', 
+                #             raw_query=f'Requestor.EmailAddress="{contactEmail}"')
+
+                #         # If there are any RT tickets found, save the ticket URL
+                #         if len(searchResults) > 0:
+                #             for rtTicket in searchResults:
+                #                 rtTicketID = rtTicket['numerical_id']
+                #                 rtTicketUrl = f'https://help.hmdc.harvard.edu/Ticket/Display.html?id={rtTicketID}'
+                #                 rtTicketUrlsList.append(rtTicketUrl)
+
+                #             # Use set function to deduplicate rtTicketUrlsList list and convert set to a list again
+                #             rtTicketUrlsList = list(set(rtTicketUrlsList))
+
+                #             # Convert list of ticket URLs to a string (to add to CSV file later)
+                #             rtTicketUrlsString = list_to_string(rtTicketUrlsList)
+                #         if len(searchResults) == 0:
+                #             rtTicketUrlsString = 'No RT tickets found'
+
+                # # If no RT username and password are provided...
+                # elif rtUserLogin or rtUserPassword == '':
+                #     rtTicketUrlsString = 'Not logged into RT. Provide RT username and password'
+
+                # Get all data about locks on the dataset
+                getAllLocksofDatasetApiEndpoint = f'{installationUrl}/api/datasets/:persistentId/locks?persistentId={lockedDatasetPid}'
+                allLockData = requests.get(getAllLocksofDatasetApiEndpoint).json()
+                # print('\tGetting information about all locks on the dataset')
+
+                for lock in allLockData['data']:
+                    datasetUrl = f'{installationUrl}/dataset.xhtml?persistentId={lockedDatasetPid}&version=DRAFT'
+                    reason = lock['lockType']
+                    lockedDate = convert_to_local_tz(lock['date'])
+                    userName = lock['user']
+
+                    # Use User Traces API endpoint to search for and return the DOIs of the depositor's datasets
+                    # with titles that are similar to the locked dataset's title
+                    # print('\tSearching for duplicate datasets')
+
+                    userTracesApiEndpointUrl = f'{installationUrl}/api/users/{userName}/traces'
+
+                    response = requests.get(
+                        userTracesApiEndpointUrl,
+                        headers={'X-Dataverse-key': apiKey})
+                    userTracesData = response.json()
+
+                    createdDatasetPidsList = []
+                    potentialDuplicateDatasetsList = []
+
+                    # If API endpoint fails, report failure in potentialDuplicateDatasetsString variable
+                    if userTracesData['status'] == 'ERROR':
+                        errorMessage = userTracesData['message']
+                        potentialDuplicateDatasetsString = f'Unable to find depositor\'s datasets. User traces API endpoint failed: {errorMessage}'
+
+                    # If API endpoint works but only one dataset is found, then that's the locked dataset
+                    # and there are no duplicate datasets. Report that.
+                    elif userTracesData['status'] == 'OK' and 'datasetCreator' in userTracesData['data']['traces'] and userTracesData['data']['traces']['datasetCreator']['count'] == 1:
+                        potentialDuplicateDatasetsString = 'No duplicate datasets found'
+
+                    # If API endpoint works and more than one dataset is found, then get the titles of
+                    # those datasets, use fuzzywuzzy library to return any titles that are close to
+                    # the title of the locked dataset
+                    elif userTracesData['status'] == 'OK' and 'datasetCreator' in userTracesData['data']['traces'] and userTracesData['data']['traces']['datasetCreator']['count'] > 1:
+                        for item in userTracesData['data']['traces']['datasetCreator']['items']:
+                            createdDatasetPid = item['pid']
+                            if createdDatasetPid != lockedDatasetPid:
+                                createdDatasetPidsList.append(createdDatasetPid)
+
+                        datasetTitles = []
+                        
+                        for createdDatasetPid in createdDatasetPidsList:
+                            datasetMetadata = get_dataset_metadata_export(
+                                installationUrl=installationUrl, datasetPid=createdDatasetPid, 
+                                exportFormat='dataverse_json', header={}, apiKey=apiKey)
+
+                            # Get title of latest version of the dataset
+                            if 'latestVersion' in datasetMetadata['data']:
+                                for field in datasetMetadata['data']['latestVersion']['metadataBlocks']['citation']['fields']:
+                                    if field['typeName'] == 'title':
+                                        datasetTitle = field['value']
+                                        datasetTitles.append(datasetTitle)
+                                        tokenSetScore = fuzz.token_set_ratio(lockedDatasetTitle, datasetTitle)
+                                        if tokenSetScore >= 80:
+                                            potentialDuplicateDatasetsList.append(createdDatasetPid)
+                        if len(potentialDuplicateDatasetsList) == 0:
+                            potentialDuplicateDatasetsString = 'No duplicate datasets found'
+                        elif len(potentialDuplicateDatasetsList) > 0:
+                            potentialDuplicateDatasetsString = list_to_string(potentialDuplicateDatasetsList)
+
+                    # Write information to the CSV file
+                    f.writerow([
+                        datasetUrl, lockedDatasetTitle, reason, lockedDate, userName,
+                        contactEmailsString, potentialDuplicateDatasetsString])
+
+                    # print('\tInformation added to CSV file')
+
+
+def unlock_dataset(installationUrl, datasetPid, apiKey):
+    unlockDatasetApiEndpointUrl = f'{installationUrl}/api/datasets/:persistentId/locks?persistentId={datasetPID}'
+    req = requests.delete(
+        destroyDatasetApiEndpointUrl,
+        headers={'X-Dataverse-key': apiKey})
+    data = req.json()
+
+    status = data.get('status')
+
+    if status:
+        message = data.get('message', '')
+        statusMessage = '%s: %s' % (status, message)
+        return statusMessage
+
+
+def unlock_datasets(
+    rootWindow, progressLabel, progressText, notUnlockedText, notUnlockedLabel,
+    installationUrl, datasetPidString, apiKey):
+    
+    installationUrl = get_installation_url(installationUrl)
+    
+    # Change passed datasetPidString to a list. Make sure the last newline doesn't mess up the list
+    datasetPidList = [x.strip() for x in datasetPidString.splitlines()]
+
+    # Remove any empty items from the list of dataset PIDs
+    datasetPidList = [datasetPid for datasetPid in datasetPidList if datasetPid]
+
+    canonicalPidList = []
+    for datasetPid in datasetPidList:
+        canonicalPid = get_canonical_pid(datasetPid)
+        canonicalPidList.append(canonicalPid)
+
+    # Delete any message in the tkinter window about datasets not being unlocked
+    # the last time the "Unlock datasets" button was pressed
+    notUnlockedLabel.grid_forget()
+
+    unlockedDatasetCount = 0
+    datasetTotalCount = len(canonicalPidList)
+
+    unlockedText = f'Datasets unlocked: 0 of {datasetTotalCount}'
+    progressText.set(unlockedText)
+    progressLabel.config(fg='green')
+    progressLabel.grid(sticky='w', row=1)
+    notUnlockedLabel.config(fg='white')
+    notUnlockedLabel.grid(sticky='w', row=2)
+    rootWindow.update_idletasks()
+
+    unlockedDatasets = []
+    notUnlockedDatasets = []
+
+    for canonicalPid in canonicalPidList:
+        
+        statusMessage = unlock_dataset(installationUrl, canonicalPid, apiKey)
+        
+        if 'OK' in statusMessage:
+            unlockedDatasets.append(canonicalPid)
+            unlockedDatasetCount += 1
+            unlockedText = f'Datasets unlocked: {unlockedDatasetCount} of {datasetTotalCount}'
+            progressText.set(unlockedText)
+            rootWindow.update_idletasks()
+
+        elif 'ERROR' in statusMessage:
+            notUnlockedLabel.config(fg='red')
+            notUnlockedDatasets.append(canonicalPid)
+            notUnlockedMessage = f'Datasets not unlocked: {len(notUnlockedDatasets)}'
+            notUnlockedText.set(notUnlockedMessage)
+            rootWindow.update_idletasks()
+
+
